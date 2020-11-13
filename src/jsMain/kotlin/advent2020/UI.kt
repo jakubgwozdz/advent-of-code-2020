a5f1eae7e49fc63baf2f879ac689388427296a75
Jakub Gwóźdź <jakub.gwozdz@schibsted.com>
2020-11-13 17:38:49 +0100

    preparation

--- src/jsMain/kotlin/advent2020/UI.kt
+++ src/jsMain/kotlin/advent2020/UI.kt
@@ -1,25 +1,57 @@
 package advent2020
 
 import kotlinx.browser.document
+import kotlinx.html.TagConsumer
 import kotlinx.html.dom.append
 import kotlinx.html.js.a
 import kotlinx.html.js.div
 import kotlinx.html.js.h1
+import kotlinx.html.js.nav
 import kotlinx.html.js.p
 import kotlinx.html.js.section
 import kotlinx.html.js.strong
+import kotlinx.html.js.title
+import org.w3c.dom.HTMLElement
 
-fun createUI() {
-    document.body!!.append {
-        section("section") {
-            div("container") {
-                h1("title") { +"Hello There" }
-                p("subtitle") {
-                    +"Well, "
-                    strong { +"Kotlin" }
-                    +" works!"
+fun createUI(day: Int?) {
+    if (day==null) {
+        document.head!!.append {
+            title { +"Advent of Code 2020" }
+        }
+        document.body!!.append {
+            section("section") {
+                div("container") {
+                    h1("title") { +"Advent of Code 2020" }
+                    p("subtitle") {
+                        +"Well, "
+                        strong { +"Kotlin-JS" }
+                        +" works!"
+                    }
+                    dayLink(1, "Chronal Calibration")
                 }
-                a(classes = "button is-primary") { +"Button" }
+            }
+        }
+    } else {
+        document.head!!.append {
+            title { +"Day $day of Advent of Code 2020" }
+        }
+    }
+}
+
+fun TagConsumer<HTMLElement>.dayLink(i: Int, title: String) {
+    nav("level is-mobile") {
+        div("level-left") {
+            div("level-item") {
+                div("field has-addons") {
+                    p("control") {
+                        a("day${i.toString().padStart(2, '0')}", classes = "button") { +"Day $i" }
+                    }
+                }
+            }
+            div("level-item") {
+                p("subtitle is-5") {
+                    +title
+                }
             }
         }
     }
--- src/jsMain/kotlin/main.kt
+++ src/jsMain/kotlin/main.kt
@@ -1,6 +1,14 @@
 import advent2020.createUI
 import kotlinx.browser.document
+import kotlinx.browser.window
+
+val funfunfun = ::createUI
 
 fun main() {
-    document.addEventListener("DOMContentLoaded", { createUI() })
+    val day = Regex("(.*/)?day(\\d+)(/.*)?")
+        .matchEntire(window.location.pathname)
+        ?.destructured?.component2()?.toInt()
+        .also { console.log(it) }
+    console.log(funfunfun)
+    document.addEventListener("DOMContentLoaded", { createUI(day) })
 }
