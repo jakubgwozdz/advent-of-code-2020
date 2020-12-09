f2a373576f6ec29cc57af1e7490f7b86abab2102
Jakub Gwóźdź <jakub.gwozdz@schibsted.com>
2020-12-09 08:23:58 +0100

    day09 no UI

--- src/commonMain/kotlin/advent2020/day09/Day09Puzzle.kt
+++ src/commonMain/kotlin/advent2020/day09/Day09Puzzle.kt
@@ -1,18 +1,22 @@
 package advent2020.day09
 
+import kotlin.math.max
+import kotlin.math.min
+
 internal fun String.parsedData() = trim().lines().map(String::toLong)
 
-fun valid(sortedPreamble: List<Long>, expectedSum: Long): Boolean {
+fun valid(preamble: List<Long>, expectedSum: Long): Boolean {
+
+    val sortedPreamble = preamble.sorted()
     var startIndex = 0
     var endIndex = sortedPreamble.lastIndex
 
     while (startIndex < endIndex) {
         val sum = sortedPreamble[startIndex] + sortedPreamble[endIndex]
         when {
-            sum == expectedSum -> return true
             sum < expectedSum -> startIndex++
             sum > expectedSum -> endIndex--
-            else -> error("wtf")
+            else -> return true
         }
     }
 
@@ -22,7 +26,7 @@ fun valid(sortedPreamble: List<Long>, expectedSum: Long): Boolean {
 fun firstInvalid(data: List<Long>, preambleLength: Int): Long {
 
     (preambleLength..data.lastIndex).forEach { index ->
-        val preamble = data.subList(index - preambleLength, index).sorted()
+        val preamble = data.subList(index - preambleLength, index)
         val entry = data[index]
         if (!valid(preamble, entry)) return entry
     }
@@ -31,20 +35,20 @@ fun firstInvalid(data: List<Long>, preambleLength: Int): Long {
 }
 
 fun contiguous(data: List<Long>, expectedSum: Long): Long {
-    (0..data.lastIndex - 2).forEach { start ->
-        var min = data[start]
-        var max = data[start]
+    (0..data.size - 2).forEach { start ->
+        var minimum = data[start]
+        var maximum = data[start]
         var sum = data[start]
         var end = start + 1
 
         while (sum < expectedSum || end < start + 2) {
             sum += data[end]
-            min = min.coerceAtMost(data[end])
-            max = max.coerceAtLeast(data[end])
+            minimum = min(minimum, data[end])
+            maximum = max(maximum, data[end])
             end++
         }
 
-        if (sum == expectedSum) return min + max
+        if (sum == expectedSum) return minimum + maximum
     }
     error("answer not found")
 }
