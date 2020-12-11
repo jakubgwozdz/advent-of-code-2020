b03229fe419872e55a1314645ac63ce460df07f0
Jakub Gwóźdź <jakub.gwozdz@schibsted.com>
2020-12-11 17:42:09 +0100

    day11 alternating random-access boolean arrays

--- src/commonMain/kotlin/advent2020/day11/Day11Puzzle.kt
+++ src/commonMain/kotlin/advent2020/day11/Day11Puzzle.kt
@@ -2,23 +2,23 @@ package advent2020.day11
 
 import advent2020.utils.atLeast
 
-fun part1(input: String) = common(input, Grid::adjacent, 4)
-fun part2(input: String) = common(input, Grid::visible, 5)
+fun part1(input: String) = common(input, Graph::adjacent, 4)
+fun part2(input: String) = common(input, Graph::visible, 5)
 
 val directions = (-1..1).flatMap { dr -> (-1..1).map { dc -> dr to dc } } - (0 to 0)
-typealias Grid = Map<Pos, Boolean>
+typealias Graph = Map<Pos, Boolean>
 typealias Pos = Pair<Int, Int>
 
-fun Grid.adjacent(pos: Pos) = directions.mapNotNull { (dr, dc) ->
+fun Graph.adjacent(pos: Pos) = directions.mapNotNull { (dr, dc) ->
     val p = pos.first + dr to pos.second + dc
     if (p in this) p
     else null
 }
 
-fun Grid.visible(pos: Pos) = directions.mapNotNull { (dr, dc) ->
+fun Graph.visible(pos: Pos) = directions.mapNotNull { (dr, dc) ->
     var r = pos.first + dr
     var c = pos.second + dc
-    while (c in (0..100) && r in (0..100)) { // TODO min - max better
+    while (c in (0..100) && r in (0..100)) {
         val p = r to c
         if (p in this) return@mapNotNull p
         r += dr
@@ -27,11 +27,11 @@ fun Grid.visible(pos: Pos) = directions.mapNotNull { (dr, dc) ->
     return@mapNotNull null
 }
 
-fun Grid.occupied(pos: Pos) = this[pos] ?: false
+// TODO hardcoded constraint on grid size 100x100.
+val Pos.index get() = first * 100 + second
 
-
-fun common(input: String, interesting: Grid.(Pos) -> List<Pos>, leaveThreshold: Int): String {
-    var grid = input.trim().lines()// parsing input
+fun common(input: String, interesting: Graph.(Pos) -> List<Pos>, leaveThreshold: Int): String {
+    val graph = input.trim().lines()// parsing input
         .flatMapIndexed { row, line ->
             line.mapIndexedNotNull { column, c ->
                 val pos: Pos = row to column
@@ -43,31 +43,31 @@ fun common(input: String, interesting: Grid.(Pos) -> List<Pos>, leaveThreshold: 
             }
         }.toMap()
 
-    val cache = grid.mapValues { (k,v)->grid.interesting(k) }
+    val cache = graph.mapValues { (k, v) -> graph.interesting(k) }
 
-//    val even = BooleanArray(10000)
-//    val odd = BooleanArray(10000)
-//
-//    grid.forEach { (k,v)->even }
+    val even = BooleanArray(10000)
+    val odd = BooleanArray(10000)
+
+    graph.forEach { (k, v) -> even[k.index] = v }
 
     var count = 0
-    var changes = 0
-    while (true) {
-        count++
-        var changed = false
-        val newGrid = grid.mapValues { (pos, occupied) ->
-            when {
-                occupied && cache[pos]!!.atLeast(leaveThreshold) { grid[it]!! } -> false.also { changes++ ; changed = true }
-                !occupied && cache[pos]!!.none { grid[it]!! } -> true.also { changes++ ; changed = true }
-                else -> occupied
-            }
 
+    while (true) {
+
+        val (src, dest) = if (count % 2 == 0) even to odd else odd to even
+        val occupied = { p: Pos -> src[p.index] }
+
+        cache.forEach { (pos, list) ->
+            dest[pos.index] = when (occupied(pos)) {
+                true -> !list.atLeast(leaveThreshold, occupied)
+                false -> list.none(occupied)
+            }
         }
-        if (!changed) {
-            println("$count iter, $changes switches, ${grid.size} seats")
-            return grid.values.count { it }.toString()
-        } else {
-            grid = newGrid
+
+        count++
+
+        if (dest.contentEquals(src)) {
+            return graph.keys.count { dest[it.index] }.toString()
         }
     }
 }
