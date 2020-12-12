930f7b311287a2fdbe2cd54c2eda18ed7a6b6b91
Jakub Gwóźdź <jakub.gwozdz@schibsted.com>
2020-12-12 07:47:53 +0100

    day12 no UI

--- src/commonMain/kotlin/advent2020/day12/Day12Puzzle.kt
+++ src/commonMain/kotlin/advent2020/day12/Day12Puzzle.kt
@@ -11,110 +11,67 @@ import kotlin.math.absoluteValue
 
 enum class Actions { N, S, E, W, L, R, F }
 
-fun rotate(dir: Actions, count: Int): Actions {
-    return if (count > 0)
-        when (dir) {
-            N -> rotate(W, count - 90)
-            S -> rotate(E, count - 90)
-            E -> rotate(N, count - 90)
-            W -> rotate(S, count - 90)
-            else -> error("dir is $dir")
-        } else if (count < 0)
-        when (dir) {
-            N -> rotate(E, count + 90)
-            S -> rotate(W, count + 90)
-            E -> rotate(S, count + 90)
-            W -> rotate(N, count + 90)
-            else -> error("dir is $dir")
-        } else dir
+internal operator fun Pair<Int,Int>.times(count:Int) = first * count to second * count
+internal operator fun Pair<Int,Int>.plus(vector: Pair<Int, Int>) = first + vector.first to second + vector.second
+internal operator fun Pair<Int,Int>.unaryMinus() = -first to -second
 
+internal fun Pair<Int, Int>.rotateLeft(count: Int) = when (count % 360) {
+    0 -> this
+    90 -> -second to first
+    180 -> -this
+    270 -> second to -first
+    else -> error("can't rotate $count")
 }
 
+internal fun Pair<Int, Int>.rotateRight(count: Int) = when (count % 360) {
+    0 -> this
+    90 -> second to -first
+    180 -> -this
+    270 -> -second to first
+    else -> error("can't rotate $count")
+}
+
+internal fun parseAsSequence(input: String) =
+    input.trim().lineSequence().map { Actions.valueOf(it.take(1)) to it.drop(1).toInt() }
+
+
+val directions = mapOf(N to (0 to 1), S to (0 to -1), E to (1 to 0), W to (-1 to 0))
+
 fun part1(input: String): String {
-    val lines = input.trim().lines()
 
-    var dir = E
-    var x = 0
-    var y = 0
-    lines.forEach { l ->
-        val a = Actions.valueOf(l.take(1))
-        val c = l.drop(1).toInt()
+    var pos = 0 to 0
+    var vector = 1 to 0
 
-        when {
-            a == N -> y -= c
-            a == S -> y += c
-            a == E -> x += c
-            a == W -> x -= c
-            a == L && c == 90 || a == R && c == 270 -> dir = when (dir) {
-                N -> W
-                S -> E
-                E -> N
-                W -> S
-                else -> error("dir is $dir")
-            }
-            a == R && c == 90 || a == L && c == 270 -> dir = when (dir) {
-                N -> E
-                S -> W
-                E -> S
-                W -> N
-                else -> error("dir is $dir")
-            }
-            a == L && c == 180 || a == R && c == 180 -> dir = when (dir) {
-                N -> S
-                S -> N
-                E -> W
-                W -> E
-                else -> error("dir is $dir")
-            }
-            a == F -> when (dir) {
-                N -> y -= c
-                S -> y += c
-                E -> x += c
-                W -> x -= c
-                else -> error("dir is $dir")
-            }
-            else -> error("invalid $l")
+    parseAsSequence(input).forEach { (action, count) ->
+        when (action) {
+            N, S, E, W -> pos += directions[action]!! * count
+            L -> vector = vector.rotateLeft(count)
+            R -> vector = vector.rotateRight(count)
+            F -> pos += vector * count
         }
+//        println("$l: ship at $pos ; v = $vector")
 
     }
-    return (x.absoluteValue + y.absoluteValue).toString()
+    return (pos.first.absoluteValue + pos.second.absoluteValue).toString()
 }
 
 fun part2(input: String): String {
-    val lines = input.trim().lines()
 
-    var x = 0
-    var y = 0
-    var wx = 10
-    var wy = -1
-    lines.forEach { l ->
-        val action = Actions.valueOf(l.take(1))
-        val count = l.drop(1).toInt()
+    var pos = 0 to 0
+    var vector = 10 to 1
+
+    parseAsSequence(input).forEach { (action, count) ->
 
         when (action) {
-            N -> wy -= count
-            S -> wy += count
-            E -> wx += count
-            W -> wx -= count
-            L -> when (count) {
-                90 -> (wy to -wx).let { wx = it.first; wy = it.second }
-                180 -> (-wx to -wy).let { wx = it.first; wy = it.second }
-                270 -> (-wy to wx).let { wx = it.first; wy = it.second }
-                else -> error("can't rotate $l")
-            }
-            R -> when (count) {
-                90 -> (-wy to wx).let { wx = it.first; wy = it.second }
-                180 -> (-wx to -wy).let { wx = it.first; wy = it.second }
-                270 -> (wy to -wx).let { wx = it.first; wy = it.second }
-                else -> error("can't rotate $l")
-            }
-            F -> {
-                x += wx * count;
-                y += wy * count
-            }
+            N, S, E, W -> vector += directions[action]!! * count
+            L -> vector = vector.rotateLeft(count)
+            R -> vector = vector.rotateRight(count)
+            F -> pos += vector * count
         }
-//        println("$l: ship to $x,$y ; wp to $wx,$wy")
+//        println("$l: ship at $pos ; v = $vector")
+
     }
-    return (x.absoluteValue + y.absoluteValue).toString()
+    return (pos.first.absoluteValue + pos.second.absoluteValue).toString()
 }
+
 
