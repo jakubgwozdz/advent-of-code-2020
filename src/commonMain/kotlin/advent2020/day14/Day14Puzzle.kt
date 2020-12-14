4c4c834a993b42b2fc630345c7694f09ee1ac197
Jakub Gwóźdź <jakub.gwozdz@schibsted.com>
2020-12-14 08:27:39 +0100

    day 14

--- src/commonMain/kotlin/advent2020/day14/Day14Puzzle.kt
+++ src/commonMain/kotlin/advent2020/day14/Day14Puzzle.kt
@@ -5,18 +5,15 @@ fun part1(input: String): String {
 
     val memory = mutableMapOf<Long, Long>()
     var andMask = "111111111111111111111111111111111111".toLong(2)
-    var orMask =  "000000000000000000000000000000000000".toLong(2)
+    var orMask = "000000000000000000000000000000000000".toLong(2)
 
     lines.forEach { line ->
-        val newMask = """mask = ([01X]{36})""".toRegex().matchEntire(line)?.destructured?.component1()
-        if (newMask != null) {
-            andMask = newMask.replace('X','1').toLong(2)
-            orMask = newMask.replace('X','0').toLong(2)
-        }
-        else {
-            val memOp =
-                """mem\[(\d+)\] = (\d+)""".toRegex().matchEntire(line)?.destructured ?: error("Invalid line $line")
-            val (addr, value) = memOp.let { (a, v) -> a.toLong() to v.toLong() }
+        val mask = mask(line)
+        if (mask != null) {
+            andMask = mask.replace('X', '1').toLong(2)
+            orMask = mask.replace('X', '0').toLong(2)
+        } else {
+            val (addr, value) = memOp(line) ?: error("Invalid line $line")
             val result = (value and andMask) or orMask
             memory[addr] = result
         }
@@ -30,49 +27,43 @@ fun part2(input: String): String {
     val lines = input.trim().lineSequence()
 
     val memory = mutableMapOf<Long, Long>()
-    var mask = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+    var masks = emptyList<Pair<Long, Long>>()
 
     lines.forEachIndexed { i, line ->
-        println("${(i+1).toString().padStart(4)}: $line")
-        val newMask = """mask = ([01X]{36})""".toRegex().matchEntire(line)?.destructured?.component1()
-        if (newMask != null) mask = newMask
-        else {
-            val memOp =
-                """mem\[(\d+)\] = (\d+)""".toRegex().matchEntire(line)?.destructured ?: error("Invalid line $line")
-            val (addr, value) = memOp.let { (a, v) -> a.toLong() to v.toLong() }
-
-            floats(addr, mask)
-//                .onEach{ addr1 -> println("memory[$addr1] = $value"}
-                .forEach{ addr1 -> memory[addr1] = value}
-
+        val newMask = mask(line)
+        if (newMask != null) {
+            masks = sequence { floats(newMask) }.toList()
+        } else {
+            val (addr, value) = memOp(line) ?: error("Invalid line $line")
+            masks.map { (andMask, orMask) -> (addr and andMask) or orMask }
+                .forEach { addr1 -> memory[addr1] = value }
         }
-
     }
 
     return memory.values.sum().toString()
 }
 
-fun floats(addr:Long, mask:String) : Sequence<Long> = sequence {
+val maskRegex = """mask = ([01X]{36})""".toRegex()
+
+private fun mask(line: String) = maskRegex.matchEntire(line)
+    ?.destructured
+    ?.component1()
+
+val memOpRegex = """mem\[(\d+)\] = (\d+)""".toRegex()
+private fun memOp(line: String) = memOpRegex.matchEntire(line)
+    ?.destructured
+    ?.let { (a, v) -> a.toLong() to v.toLong() }
+
+
+private suspend fun SequenceScope<Pair<Long, Long>>.floats(mask: String) {
     val index = mask.indexOf('X')
     if (index < 0) {
-        val bits = addr.toString(2).padStart(mask.length, '0')
-        val masked = bits.mapIndexed { index, c ->
-            when (mask[index]) {
-                '_' -> '0'
-                '0' -> c
-                '1' -> '1'
-                else -> error("invalid mask $mask")
-            }
-        }.toCharArray().concatToString()
-//        println("mask $mask changed address $addr ($bits) to ${masked.toLong(2)} ($masked)")
-        yield(masked.toLong(2))
+        val andMask = mask.replace('0', '1').replace('_', '0').toLong(2)
+        val orMask = mask.replace('0', '0').replace('_', '0').toLong(2)
+
+        yield(andMask to orMask)
     } else {
-        val newMask0 = mask.replaceRange(index, index+1,"_")
-        val newMask1 = mask.replaceRange(index, index+1,"1")
-
-        yieldAll(floats(addr, newMask0))
-        yieldAll(floats(addr, newMask1))
+        floats(mask.replaceRange(index, index + 1, "_"))
+        floats(mask.replaceRange(index, index + 1, "1"))
     }
-
-
 }
