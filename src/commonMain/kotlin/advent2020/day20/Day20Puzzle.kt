5f1c446165800c8489ad7c9ab4dec59c96827f9d
Jakub Gwóźdź <jakub.gwozdz@schibsted.com>
2020-12-20 13:22:46 +0100

    day 20 part 2 ugly af

--- src/commonMain/kotlin/advent2020/day20/Day20Puzzle.kt
+++ src/commonMain/kotlin/advent2020/day20/Day20Puzzle.kt
@@ -10,6 +10,7 @@ data class Match(val edge1: Edge, val edge2: Edge, val flip: Boolean)
 fun List<Char>.asInt() = map { if (it == '#') '1' else '0' }.joinToString("").toInt(2)
 
 data class Tile(val id: Long, val lines: List<String>) {
+    val size = lines.count()
     val top = lines.first().toList().asInt()
     val topR = lines.first().toList().reversed().asInt()
 
@@ -25,7 +26,16 @@ data class Tile(val id: Long, val lines: List<String>) {
     fun at(y: Int, x: Int, o: Orientation): Char {
         val (topEdge, flip) = o
         return when {
+            x !in lines.indices -> ' '
+            y !in lines.indices -> ' '
             topEdge == Edge.top && !flip -> lines[y][x]
+            topEdge == Edge.top && flip -> lines[y][size - 1 - x]
+            topEdge == Edge.right && flip -> lines[x][y]
+            topEdge == Edge.right && !flip -> lines[x][size - 1 - y]
+            topEdge == Edge.bottom && !flip -> lines[size - 1 - y][size - 1 - x]
+            topEdge == Edge.bottom && flip -> lines[size - 1 - y][x]
+            topEdge == Edge.left && !flip -> lines[size - 1 - x][y]
+            topEdge == Edge.left && flip -> lines[size - 1 - x][size - 1 - y]
             else -> error("unknown $o")
         }
     }
@@ -110,49 +120,49 @@ fun part2(input: String): String {
 
     // first corner
     cornerTiles.first().let { next ->
-        place(next, 0, 0, jigsaw, used, matches)
+        place(next, 0, 0, jigsaw, used)
     }
     // first of first edge
     (1).let { c ->
         val prev = jigsaw[0][c - 1]!!
         val next = matches[prev]!!.keys.first { it in edgeTiles && it !in used }
-        place(next, 0, c, jigsaw, used, matches)
+        place(next, 0, c, jigsaw, used)
     }
     // rest of first edge
     (2 until width - 1).forEach { c ->
         val prev = jigsaw[0][c - 1]!!
         val next = matches[prev]!!.keys.single { it in edgeTiles && it !in used }
-        place(next, 0, c, jigsaw, used, matches)
+        place(next, 0, c, jigsaw, used)
     }
     // second edge
     (1 until width - 1).forEach { r ->
         val prev = jigsaw[r - 1][0]!!
         val next = matches[prev]!!.keys.single { it in edgeTiles && it !in used }
-        place(next, r, 0, jigsaw, used, matches)
+        place(next, r, 0, jigsaw, used)
     }
     // second corner
     cornerTiles.first { jigsaw[0][width - 2]!! in matches[it]!! && it !in used }.let { next ->
-        place(next, 0, width - 1, jigsaw, used, matches)
+        place(next, 0, width - 1, jigsaw, used)
     }
     // third corner
     cornerTiles.first { jigsaw[width - 2][0]!! in matches[it]!! && it !in used }.let { next ->
-        place(next, width - 1, 0, jigsaw, used, matches)
+        place(next, width - 1, 0, jigsaw, used)
     }
     // third edge
     (1 until width - 1).forEach { r ->
         val prev = jigsaw[r - 1][width - 1]!!
         val next = matches[prev]!!.keys.single { it in edgeTiles && it !in used }
-        place(next, r, width - 1, jigsaw, used, matches)
+        place(next, r, width - 1, jigsaw, used)
     }
     // fourth edge
     (1 until width - 1).forEach { c ->
         val prev = jigsaw[width - 1][c - 1]!!
         val next = matches[prev]!!.keys.single { it in edgeTiles && it !in used }
-        place(next, width - 1, c, jigsaw, used, matches)
+        place(next, width - 1, c, jigsaw, used)
     }
     // fourth corner
     cornerTiles.first { jigsaw[width - 2][width - 1]!! in matches[it]!! && it !in used }.let { next ->
-        place(next, width - 1, width - 1, jigsaw, used, matches)
+        place(next, width - 1, width - 1, jigsaw, used)
     }
 
     //middle
@@ -162,57 +172,90 @@ fun part2(input: String): String {
             val onLeft = jigsaw[r][c - 1]!!
             val next =
                 matches[onTop]!!.keys.intersect(matches[onLeft]!!.keys).single { it in insideTiles && it !in used }
-            place(next, r, c, jigsaw, used, matches)
+            place(next, r, c, jigsaw, used)
         }
     }
 
-    val tileSize = tiles.values.first().lines.size
-    val image = Array(tileSize * width) { Array(tileSize * width) { '.' } }
+    val tileSize = 10
+    val image = Array((tileSize - 1) * width + 1) { Array((tileSize - 1) * width + 1) { ' ' } }
 
     // place
     jigsaw.indices.forEach { r ->
         jigsaw[r].indices.forEach { c ->
-            val orientation = Edge.values().flatMap { listOf(Orientation(it, false), Orientation(it, true)) }
-                .first { o -> true }
+            val tile = jigsaw[r][c]!!
+            val actTop = if (r - 1 in jigsaw.indices) jigsaw[r - 1][c] else null
+            val actBottom = if (r + 1 in jigsaw.indices) jigsaw[r + 1][c] else null
+            val actLeft = if (c - 1 in jigsaw[r].indices) jigsaw[r][c - 1] else null
+            val actRight = if (c + 1 in jigsaw[r].indices) jigsaw[r][c + 1] else null
+            val m = matches[tile]!!
+            val expTop = m.entries.singleOrNull { it.value.edge1 == Edge.top }?.key
+            val expBottom = m.entries.singleOrNull { it.value.edge1 == Edge.bottom }?.key
+            val expLeft = m.entries.singleOrNull { it.value.edge1 == Edge.left }?.key
+            val expRight = m.entries.singleOrNull { it.value.edge1 == Edge.right }?.key
+            val act = listOf(actTop, actRight, actBottom, actLeft)
+            val orientation =
+                Edge.values().flatMap { listOf(Orientation(it, false), Orientation(it, true)) }
+                    .single { (t, flip) ->
+                        when {
+                            t == Edge.top && !flip -> act == listOf(expTop, expRight, expBottom, expLeft)
+                            t == Edge.top && flip -> act == listOf(expTop, expLeft, expBottom, expRight)
+                            t == Edge.right && !flip -> act == listOf(expRight, expBottom, expLeft, expTop)
+                            t == Edge.right && flip -> act == listOf(expLeft, expBottom, expRight, expTop)
+                            t == Edge.bottom && !flip -> act == listOf(expBottom, expLeft, expTop, expRight)
+                            t == Edge.bottom && flip -> act == listOf(expBottom, expRight, expTop, expLeft)
+                            t == Edge.left && !flip -> act == listOf(expLeft, expTop, expRight, expBottom)
+                            t == Edge.left && flip -> act == listOf(expRight, expTop, expLeft, expBottom)
+                            else -> error("wtf")
+                        }
+                    }
 
-            (0 until tileSize).forEach { y ->
-                (0 until tileSize).forEach { x ->
-                    image[r * tileSize + y][c * tileSize + x] = tiles[jigsaw[r][c]!!]!!.at(y, x, orientation)
+            (1 until tileSize - 1).forEach { y ->
+                (1 until tileSize - 1).forEach { x ->
+                    val at = tiles[tile]!!.at(y, x, orientation)
+                    image[r * (tileSize - 2) + y][c * (tileSize - 2) + x] = at
                 }
             }
         }
     }
 
-    image.map { line -> line.joinToString("") }.forEach { println(it) }
+    val full = Tile(0, image.map { line -> line.joinToString("") })
 
+//    val str = "                  # \n#    ##    ##    ###\n #  #  #  #  #  #   "
 
-    TODO("fsadfasfas")
+    val monsters = Edge.values().flatMap { listOf(Orientation(it, false), Orientation(it, true)) }
+        .sumBy { o ->
+            println("testing $o")
+            (0..image.size).sumBy { y ->
+                (0..image.size).count { x ->
+                    full.at(y, x + 18, o) == '#' &&
+                            full.at(y + 1, x, o) == '#' &&
+                            full.at(y + 1, x + 5, o) == '#' &&
+                            full.at(y + 1, x + 6, o) == '#' &&
+                            full.at(y + 1, x + 11, o) == '#' &&
+                            full.at(y + 1, x + 12, o) == '#' &&
+                            full.at(y + 1, x + 17, o) == '#' &&
+                            full.at(y + 1, x + 18, o) == '#' &&
+                            full.at(y + 1, x + 19, o) == '#' &&
+                            full.at(y + 2, x + 1, o) == '#' &&
+                            full.at(y + 2, x + 4, o) == '#' &&
+                            full.at(y + 2, x + 7, o) == '#' &&
+                            full.at(y + 2, x + 10, o) == '#' &&
+                            full.at(y + 2, x + 13, o) == '#' &&
+                            full.at(y + 2, x + 16, o) == '#'
+                }
+            }
+                .also { println("count $it") }
+        }
+
+    val hashes = image.sumBy { it.count { c -> c == '#' } }
+
+    val result = hashes - 15 * monsters
+
+    return result.toString()
 }
 
-private fun place(
-    next: Long,
-    row: Int,
-    col: Int,
-    jigsaw: Array<Array<Long?>>,
-    used: MutableSet<Long>,
-    matches: Map<Long, Map<Long, Match>>
-) {
+private fun place(next: Long, row: Int, col: Int, jigsaw: Array<Array<Long?>>, used: MutableSet<Long>) {
     check(next !in used) { "$next already used" }
-//    println("placing $next @ $row,$col. knows ${matches[next]}")
-//    if (row - 1 in jigsaw.indices) {
-//        val prev = jigsaw[row - 1][col]
-//        if (prev != null) {
-//            val match = matches[prev]!![next]!!
-//            println("$next @ $row,$col matches bottom of ${prev}($match)")
-//        }
-//    }
-//    if (col - 1 in jigsaw[row].indices) {
-//        val prev = jigsaw[row][col - 1]
-//        if (prev != null) {
-//            val match = matches[prev]!![next]!!
-//            println("$next @ $row,$col matches right of ${prev}($match)")
-//        }
-//    }
     jigsaw[row][col] = next
     used += next
 }
