76d05d107e2a1f7aca75044fa21e58cdec245499
Jakub Gwóźdź <jakub.gwozdz@schibsted.com>
2020-12-22 07:29:02 +0100

    day 22 working in JVM

--- src/commonMain/kotlin/advent2020/day22/Day22Puzzle.kt
+++ src/commonMain/kotlin/advent2020/day22/Day22Puzzle.kt
@@ -1,14 +1,67 @@
 package advent2020.day22
 
-fun part1(input: String): String {
-    val lines = input.trim().lines()
+import advent2020.utils.groups
 
-    TODO()
+fun part1(input: String): String {
+    val (p1, p2) = decks(input)
+    combat(p1, p2)
+    return score(p1, p2).toString()
 }
 
 fun part2(input: String): String {
-    val lines = input.trim().lines()
-
-    TODO()
+    val (p1, p2) = decks(input)
+    recursiveCombat(p1, p2)
+    return score(p1, p2).toString()
 }
+
+fun decks(input: String): Pair<MutableList<Int>, MutableList<Int>> = input.trim()
+    .lineSequence()
+    .groups { it.isBlank() }
+    .map { l -> l.drop(1).map { it.toInt() }.toMutableList() }
+    .toList()
+    .let { (p1, p2) -> p1 to p2 }
+
+fun score(p1: List<Int>, p2: List<Int>): Long {
+    val p1sum = p1.reversed().mapIndexed { index, i -> (index + 1).toLong() * i }.sum()
+    val p2sum = p2.reversed().mapIndexed { index, i -> (index + 1).toLong() * i }.sum()
+    return p1sum + p2sum
+}
+
+fun combat(p1: MutableList<Int>, p2: MutableList<Int>) {
+    while (p1.isNotEmpty() && p2.isNotEmpty()) {
+        val card1 = p1.removeFirst()
+        val card2 = p2.removeFirst()
+        when {
+            card1 > card2 -> p1 += listOf(card1, card2)
+            card2 > card1 -> p2 += listOf(card2, card1)
+            else -> error("same card $card1")
+        }
+    }
+}
+
+fun recursiveCombat(p1: MutableList<Int>, p2: MutableList<Int>): Int {
+    val memory = mutableSetOf<Pair<List<Int>, List<Int>>>()
+    while (p1.isNotEmpty() && p2.isNotEmpty()) {
+        val state = p1.toList() to p2.toList()
+        if (state in memory) return 1
+        memory += state
+        val card1 = p1.removeFirst()
+        val card2 = p2.removeFirst()
+        val winner = when {
+            card1 <= p1.size && card2 <= p2.size -> recursiveCombat(
+                p1.take(card1).toMutableList(),
+                p2.take(card2).toMutableList(),
+            )
+            card1 > card2 -> 1
+            card1 < card2 -> 2
+            else -> error("same card $card1")
+        }
+        when (winner) {
+            1 -> p1 += listOf(card1, card2)
+            2 -> p2 += listOf(card2, card1)
+        }
+    }
+    return if (p1.isNotEmpty()) 1 else 2
+}
+
 
--- src/commonTest/kotlin/advent2020/day22/Day22PuzzleTest.kt
+++ src/commonTest/kotlin/advent2020/day22/Day22PuzzleTest.kt
@@ -2,7 +2,6 @@ package advent2020.day22
 
 import advent2020.readResource
 import advent2020.runTest
-import advent2020.runTestExpect
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -12,30 +11,56 @@ class Day22PuzzleTest {
 
     @Test
     fun examplePart1() = runTest {
-        val sampleInput = """"""
+        val sampleInput = """
+            Player 1:
+            9
+            2
+            6
+            3
+            1
+            
+            Player 2:
+            5
+            8
+            4
+            7
+            10
+            """.trimIndent()
         val actual = part1(sampleInput)
-        assertEquals("", actual)
+        assertEquals("306", actual)
     }
 
     @Test
-    fun examplePart2() = runTestExpect(NotImplementedError::class) {
-        val sampleInput = """"""
+    fun examplePart2() = runTest {
+        val sampleInput = """
+            Player 1:
+            9
+            2
+            6
+            3
+            1
+            
+            Player 2:
+            5
+            8
+            4
+            7
+            10
+            """.trimIndent()
         val actual = part2(sampleInput)
-        assertEquals("", actual)
+        assertEquals("291", actual)
     }
 
     @Test
     fun jakubgwozdzPart1() = runTest {
-//    fun jakubgwozdzPart1() = runTestExpect(NotImplementedError::class) {
         val actual = part1(myPuzzleInput)
-        assertEquals("", actual)
+        assertEquals("35005", actual)
     }
 
     @Test
-//    fun jakubgwozdzPart2() = runTest {
-    fun jakubgwozdzPart2() = runTestExpect(NotImplementedError::class) {
+    fun jakubgwozdzPart2() = runTest {
         val actual = part2(myPuzzleInput)
-        assertEquals("", actual)
+        assertEquals("32751", actual)
     }
 
 }
