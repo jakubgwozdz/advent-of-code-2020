45167553d67716466d6ec18178d8923510db52d7
Jakub Gwóźdź <jakub.gwozdz@schibsted.com>
2020-12-24 08:23:06 +0100

    day24 part 2

--- src/commonMain/kotlin/advent2020/day24/Day24Puzzle.kt
+++ src/commonMain/kotlin/advent2020/day24/Day24Puzzle.kt
@@ -1,8 +1,5 @@
 package advent2020.day24
 
-import kotlin.math.max
-import kotlin.math.min
-
 enum class Vector(val dx: Int, val dy: Int) {
     E(2, 0),
     W(-2, 0),
@@ -33,39 +30,17 @@ fun part2(input: String): String {
         .map { line -> tile(line) }
         .fold(emptySet<Position>()) { acc, tile -> if (tile in acc) acc - tile else acc + tile }
 
-    var minX = 0
-    var maxX = 0
-    var minY = 0
-    var maxY = 0
-
-    tiles.forEach { (x, y) ->
-        minX = min(minX, x)
-        maxX = max(maxX, x)
-        minY = min(minY, y)
-        maxY = max(maxY, y)
-    }
-
     repeat(100) {
-        tiles = (minY - 1..maxY + 1)
-            .flatMap { y ->
-                val range = if ((y % 2 == 0) == (minX % 2 == 0)) {
-                    minX - 2..maxX + 2 step 2
-                } else {
-                    minX - 1..maxX + 2 step 2
-                }
-                range.map { x -> Position(x, y) }
+
+        val neighbours = mutableMapOf<Position, Int>()
+        tiles.forEach { tile ->
+            Vector.values().forEach { v ->
+                neighbours[tile + v] = (neighbours[tile + v] ?: 0) + 1
             }
-            .filter { tile ->
-                val adjacent = Vector.values().filter { s -> tile + s in tiles }.size
-                if (tile in tiles) adjacent == 1 || adjacent == 2 else adjacent == 2
-            }
-            .onEach { (x, y) ->
-                minX = min(minX, x)
-                maxX = max(maxX, x)
-                minY = min(minY, y)
-                maxY = max(maxY, y)
-            }
-            .toSet()
+        }
+        tiles = neighbours
+            .filter { (tile, count) -> if (tile in tiles) count == 1 || count == 2 else count == 2 }
+            .keys
 
     }
     println(" Day 100: ${tiles.size} ")
@@ -74,29 +49,17 @@ fun part2(input: String): String {
 }
 
 
-private fun tile(line: String) = sequence<String> {
-    var p = 0
-    while (p < line.length) {
-        when (line[p]) {
-            'e', 'w' -> yield(line.substring(p, p + 1)).also { p += 1 }
-            's', 'n' -> yield(line.substring(p, p + 2)).also { p += 2 }
-            else -> error("unknown `${line.drop(p)}` @ $p")
+private fun tile(line: String) =
+    sequence {
+        var p = 0
+        while (p < line.length) {
+            when (line[p]) {
+                'e', 'w' -> yield(line.substring(p, p + 1)).also { p += 1 }
+                's', 'n' -> yield(line.substring(p, p + 2)).also { p += 2 }
+                else -> error("unknown `${line.drop(p)}` @ $p")
+            }
         }
     }
-}
-//            .onEach { print("$it ") }
-    .map {
-        when (it) {
-            "e" -> Vector.E
-            "w" -> Vector.W
-            "se" -> Vector.SE
-            "sw" -> Vector.SW
-            "ne" -> Vector.NE
-            "nw" -> Vector.NW
-            else -> error("unknown `$it`")
-        }
-
-    }
-    .fold(Position(0, 0)) { p, v -> p + v }
-//            .also { println(it) }
+        .map { Vector.valueOf(it.toUpperCase()) }
+        .fold(Position(0, 0)) { p, v -> p + v }
 
